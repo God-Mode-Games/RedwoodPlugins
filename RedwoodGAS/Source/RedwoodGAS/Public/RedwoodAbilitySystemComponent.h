65528df52d3dc413a3f69095b81ccaef5a677164
Catapultam <247223157+Catapultam-GMG@users.noreply.github.com>
2026-06-25 11:50:48 -0500

    Revert "Don't persist activatable abilities; Redwood persists only effects + attributes"
    This reverts commit b99895e474341eed0a6b841115f27e32b226f934.

--- RedwoodGAS/Source/RedwoodGAS/Public/RedwoodAbilitySystemComponent.h
+++ RedwoodGAS/Source/RedwoodGAS/Public/RedwoodAbilitySystemComponent.h
@@ -84,6 +84,12 @@ public:
   bool bDisableAutoDeserialization = false;
 
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Redwood")
+  ERedwoodASCInclusionMode AbilityInclusionMode =
+    ERedwoodASCInclusionMode::Blacklist;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Redwood")
+  TArray<TSubclassOf<UGameplayAbility>> AbilityInclusionArray;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Redwood")
   ERedwoodASCInclusionMode EffectInclusionMode =
     ERedwoodASCInclusionMode::Blacklist;
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Redwood")
